632ec2aa91018063909e281ee31a249d77078cca
MathewTG <mathewtg.mec@gmail.com>
2018-12-08 11:17:01 +0530

    Adding Simple AND Gate functionality

---
+++ simpleand.v
@@ -0,0 +1,17 @@
+// Simple AND
+//----------------------------------
+// FileName : simpleand.v
+// Type : module
+// Author : Mathew T G
+// Contact : mathewtg.mec@gmail.com
+//------------------
+// Date : 15/10/2018
+//------------------
+// Notes :- Too Simple for a note na ?
+//
+
+module simple_and (f,x,y);
+input x,y;
+output f;
+assign f=x&y;
+endmodule
