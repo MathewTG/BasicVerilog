35481bea71de7033398d5eace4843987da6b1886
Mathew T George <mathewtg.mec@gmail.com>
2020-05-16 16:21:43 +0530

    Full adder using UDP Sum and Carry

---
+++ udp_fa.v
@@ -0,0 +1,19 @@
+// Full Adder using the User Defined Primitives
+//----------------------------------
+// FileName : udp_fa.v
+// Type : module
+// Author : Mathew T G
+// Contact : mathewtg.mec@gmail.com
+//------------------
+// Date : 18/10/20118
+//------------------
+// Notes :-
+//
+module udp_fa (sum,carry,a,b,c);
+input a,b,c;
+output sum,carry;
+
+udp_fasum SUM (sum,a,b,c);
+udp_facarry CARRY (carry,a,b,c);
+
+endmodule
