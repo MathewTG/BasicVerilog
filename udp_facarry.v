c6b5397333bb8b1998f09e1d02df4840d5bb406b
Mathew T George <mathewtg.mec@gmail.com>
2020-05-16 16:18:16 +0530

    UDP Full adder Carry

---
+++ udp_facarry.v
@@ -0,0 +1,26 @@
+// User Defined Primitive - Full Adder Carry
+//----------------------------------
+// FileName : udp_facarry.v
+// Type : module
+// Author : Mathew T G
+// Contact : mathewtg.mec@gmail.com
+//------------------
+// Date : 18/10/20118
+//------------------
+// Notes :-
+//
+primitive udp_facarry (carry,a,b,c);
+input a,b,c;
+output carry;
+
+ table 
+// a b c : carry
+   0 0 ? : 0;
+   0 ? 0 : 0;
+   ? 0 0 : 0;
+   1 1 ? : 1;
+   1 ? 1 : 1;
+   ? 1 1 : 1;
+ endtable
+
+endprimitive
