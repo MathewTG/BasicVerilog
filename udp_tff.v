2d5a4450094f541c8d642d63aff78f4cb898122b
Mathew T George <mathewtg.mec@gmail.com>
2020-05-18 09:30:07 +0530

    UDP T Flip Flop

---
+++ udp_tff.v
@@ -0,0 +1,25 @@
+// Negative edge T FF using UDP 
+//----------------------------------
+// FileName : udp_tff.v
+// Type : module
+// Author : Mathew T G
+// Contact : mathewtg.mec@gmail.com
+//------------------
+// Date : 19/10/20118
+//------------------
+// Notes :-
+//
+primitive udp_tff (q,clk,clr);
+input clk,clr;
+output reg q;
+
+table
+// clk clr : q : q_new
+   ?     1   : ? : 0;
+   ?    (10) : ? : -;
+   (10)  0   : 1 : 0;
+   (10)  0   : 0 : 1;
+   (0?)  0   : ? : -;
+endtable
+
+endprimitive
